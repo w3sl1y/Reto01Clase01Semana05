28510f8eafeafec80bec13d58ccee6b9f0c2bac9
w3sl1y <SV73623892@idat.edu.pe>
2021-09-26 20:42:34 -0500

    Actualización

---
+++ reto01semana05/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <p/>
+    <form>
+      <label htmlFor="name">Full name: </label>
+      <br/>
+      <input type="text" id="name"/>
+    </form>
+    </div>
+  );
+}
+
+export default App;
